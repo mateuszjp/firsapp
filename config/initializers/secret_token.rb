e30152a4e9615a05b2f48efb3f153afaa2f6d320
mateuszjp <mateuszjp@gmail.com>
2013-10-15 23:53:54 +0200

    firsapp deploy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firsapp::Application.config.secret_key_base = 'f30c805d08734973f233464f164f864242738ba73e2ddadd83c7749da10028490061eac0b07fcee2d38bf4d9c6a0ca1aaf998f73dd032fa37ce5cbf7403f9894'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Firsapp::Application.config.session_store :cookie_store, key: '_firsapp_session'
